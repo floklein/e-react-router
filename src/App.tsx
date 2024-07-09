b6e2905054e1ca3bdf2e50e9c8b31a5729a53f45
Florent Klein <florent.klein@dametis.com>
2024-07-10 00:28:37 +0200

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { router } from "./router";
-import { useAuth } from "./zustand/auth";
 import { RouterProvider } from "react-router-dom";
 
 export default function App() {
-  const user = useAuth((state) => state.user);
-  const isAuthenticated = useAuth((state) => state.isAuthenticated);
-
   return <RouterProvider router={router} />;
 }
